c9e72c4fea680a9e505fb96c86e6ef4cec02023e
jonathonmcclen <jonathonsmcclendon@gmail.com>
2021-07-24 18:25:01 -0500

    commit tree ef1a0f80bfa0ed85402fa12ae3401976476a117b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+      
+    if self.legal_triangle
+      
+    else
+      raise TriangleError
+    end 
+  end
+
+  def kind
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @c == @a
+      :isosceles
+    elsif @a != @b && @a != @c && @c != @b
+      :scalene
+    end 
+  end 
+
+  def legal_triangle
+    self.no_zero_sides && self.triangle_inequality
+  end 
+
+  def no_zero_sides
+    @a > 0 && @b > 0 && @c > 0
+  end 
+
+  def triangle_inequality
+    @a+@b > @c && @b+@c > @a && @c+@a > @b
+  end 
+
+  class TriangleError < StandardError
+    " "
+  end
 end
